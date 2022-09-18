22ffdfb6f9213f1a3035893389ac029d063d3bf0
Rendi Rizki <rendirizkir@gmail.com>
2022-09-18 23:36:00 +0700

    fix: environment

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   type: 'website',
   name: 'Company Profile',
-  url: 'http://localhost:3000',
+  url: process.env.NEXT_PUBLIC_SITE_URL,
   favicon: 'https://placehold.co/16x16',
   twitter: '@twitter',
 };
