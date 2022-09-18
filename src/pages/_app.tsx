8bba57390920b361c9ffb84606896ca921facfc1
Rendi Rizki <rendirizkir@gmail.com>
2022-09-19 00:07:26 +0700

    fix: partytown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        strategy="worker"
+      />
+    </>
   );
 }
 
